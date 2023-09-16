def932a8a42409025c4ea271acfb2b9f38efafb8
Alok RG <alokratan277@gmail.com>
2023-09-16 13:34:59 +0530

    formik remove

--- screens/Register.js
+++ screens/Register.js
@@ -21,8 +21,6 @@ import {
 
   
 } from "native-base";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import { ToastAndroid } from "react-native";
 import React, { useState, useEffect } from "react";
 // import CON from "../country.json";
@@ -36,64 +34,20 @@ import CountryPicker from "./components/CountryPicker";
 
 const Register = ({ navigation }) => {
   const [show, setShow] = useState(false);
-  // const [fullname, setFullname] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [phone, setPhone] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [purpose, setPurpose] = useState("");
   const [country, setCountry] = useState("");
-  // const [address, setAddress] = useState("");
-  // const [phonecode,setPhonecode]=useState("");
-  // const [pannumber,setPannumber]=useState("");
-  // const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [phonecode,setPhonecode]=useState("");
+  const [pannumber,setPannumber]=useState("");
+  const [password, setPassword] = useState("");
   const [isSelected, setIsSelected] = useState(false);
   const [openmodal,setOpenmodal]=useState(false);
   const [coun, setCoun] = useState("");
   const [countries, setCountries] = useState(Coundet);
   const [selectedCountry, setSelectedCountry] = useState('');
-
-  const formik = useFormik({
-    initialValues: {
-      fullname: "",
-      email: "",
-      password: "",
-      phone: "",        // Added phone field
-      pannumber: "",   // Added pannumber field
-      address: "",     // Added address field
-    },
-    validationSchema: Yup.object().shape({
-      fullname: Yup.string().required("Required"),
-      email: Yup.string().email("Invalid email").required("Required"),
-      password: Yup.string()
-        .required("Required")
-        .min(8, "Too Short! - should be 8 chars minimum.")
-        .matches(
-          /[a-z]/,
-          "Password must contain at least one lowercase letter."
-        )
-        .matches(
-          /[A-Z]/,
-          "Password must contain at least one uppercase letter."
-        )
-        .matches(/[0-9]/, "Password must contain at least one numeric digit.")
-        .matches(
-          /[!@#$%^&*(),.?":{}|<>]/,
-          "Password must contain at least one special character."
-        ),
-      phone: Yup.string().required("Required"),        // Added phone field validation
-      pannumber: Yup.string().required("Required")
-      .matches(/^[A-Z]{5}\d{4}[A-Z]$/, "Invalid PAN card number") // Add regex for PAN card
-      .required("Required"),  // Added pannumber field validation
-      address: Yup.string(),     // Added address field validation
-    }),
-    onSubmit: (values, { resetForm }) => {
-   
-      registerfn(values)
-      console.log(values)
-      resetForm({ values: "" });
-    },
-  });
-  
-
   useEffect(() => {
     data();   
   }, []);
@@ -113,18 +67,17 @@ const Register = ({ navigation }) => {
 
   }
 const registerfnt=()=>{console.log("please accept terms & conditions")}
-
-  const registerfn = async (values) => {
+  const registerfn = async () => {
     await axios
       .post(`${baseurl}/register`, {
-        fullname: values.fullname,
-        email: values.email,
-        mobilenumber: values.phone,
-        panno:values.pannumber,
+        fullname: fullname,
+        email: email,
+        mobilenumber: phone,
+        panno:pannumber,
         purpose: "Education",
         country: selectedCountry,
-        address:values.address,
-        userpassword: values.password,
+        address:address,
+        userpassword: password,
       })
       .then((res) => {
         console.log(res.data.message);
@@ -139,27 +92,25 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
         ToastAndroid.show(error.response.data.error, 2000);
       });
   };
-
-
-  const registerfnvin = async (values) => {
+  const registerfnvin = async () => {
     await axios
       .post(`http://65.1.104.173:443/api/common/register`, {
-        username: values.fullname,
-        email: values.email,
-        mobile: values.phone,
-        panno:values.pannumber,
+        username: fullname,
+        email: email,
+        mobile: phone,
+        panno:pannumber,
         purpose: "Education",
         countrycode: selectedCountry,
-        address:values.address,
-        password: values.password,
+        address:address,
+        password: password,
       })
       .then((res) => {
         console.log(res.data.res.Message);
 
         ToastAndroid.show(res.data.res.Message, 2000);
-        setTimeout(() => {
-          navigation.navigate("logins");
-        }, 2000);
+        // setTimeout(() => {
+        //   navigation.navigate("logins");
+        // }, 1000);
      
       })
       .catch((error) => {
@@ -170,13 +121,13 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
 
 
 
-  // const registerfn2 = async () => {
-  //   // registerfn();
-  //   registerfnvin()
-  //   console.log(fullname,email,pannumber, password, phone, selectedCountry,address);
+  const registerfn2 = async () => {
+    // registerfn();
+    registerfnvin()
+    console.log(fullname,email,pannumber, password, phone, selectedCountry,address);
 
-  //   ToastAndroid.show("Please Wait..", 2000);
-  // };
+    ToastAndroid.show("Please Wait..", 2000);
+  };
  
 
  
@@ -285,27 +236,16 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
                 padding={3}
                 placeholder="Elon Musk"
                 color="black"
-               
+                marginBottom={4}
                 fontSize={17}
-                
-                onChangeText={formik.handleChange("fullname")}
-                onBlur={formik.handleBlur("fullname")}
-                value={formik.values.fullname}
+                keyboardType="email-address"
+                value={fullname}
+                onChangeText={(e) => setFullname(e)}
                 borderBottomColor="#0B0464"
               />
-              {formik.errors.fullname && formik.touched.fullname ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.fullname}
-                </Text>
-              ) : null}
         
               <FormControl.Label paddingY={2}>
-                <Text fontWeight={600} marginTop={4} fontSize={17}>
+                <Text fontWeight={600} fontSize={17}>
                   Email
                 </Text>
               </FormControl.Label>
@@ -325,26 +265,15 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
                 padding={3}
                 placeholder="user@gmail.com"
                 color="black"
-               
+                marginBottom={4}
                 fontSize={17}
                 keyboardType="email-address"
-                onChangeText={formik.handleChange("email")}
-                onBlur={formik.handleBlur("email")}
-                value={formik.values.email}
+                value={email}
+                onChangeText={(e) => setEmail(e)}
                 borderBottomColor="#0B0464"
               />
-               {formik.errors.email && formik.touched.email ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.email}
-                </Text>
-              ) : null}
  <FormControl.Label paddingY={2}>
-                <Text fontWeight={600} marginTop={4} fontSize={17}>
+                <Text fontWeight={600} fontSize={17}>
                   Phone Number
                 </Text>
               </FormControl.Label>
@@ -371,26 +300,15 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
                 placeholder="834989384"
                 color="black"
                 keyboardType="phone-pad"
-                onChangeText={formik.handleChange("phone")}
-                onBlur={formik.handleBlur("phone")}
-                value={formik.values.phone}
-               
+                value={phone}
+                onChangeText={(e) => setPhone(e)}
+                marginBottom={4}
                 fontSize={17}
                 borderBottomColor="#0B0464"
               />
-               {formik.errors.phone && formik.touched.phone ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.phone}
-                </Text>
-              ) : null}
 
 <FormControl.Label paddingY={2}>
-                <Text fontWeight={600} marginTop={4} fontSize={17}>
+                <Text fontWeight={600} fontSize={17}>
                   Pan number
                 </Text>
               </FormControl.Label>
@@ -406,27 +324,16 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
                 padding={3}
                 placeholder="ABD7263H"
                 color="black"
-                onChangeText={formik.handleChange("pannumber")}
-                onBlur={formik.handleBlur("pannumber")}
-                value={formik.values.pannumber}
+                value={pannumber}
+                onChangeText={(e) => setPannumber(e)}
                 // onChangeText={(e) => setPhone(e)}
-               
+                marginBottom={4}
                 fontSize={17}
                 borderBottomColor="#0B0464"
               />
-                {formik.errors.pannumber && formik.touched.pannumber ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.pannumber}
-                </Text>
-              ) : null}
              
               <FormControl.Label paddingY={2}>
-                <Text marginTop={4} fontWeight={600} fontSize={17}>
+                <Text fontWeight={600} fontSize={17}>
                   Purpose
                 </Text>
               </FormControl.Label>
@@ -461,29 +368,18 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
               
                 padding={3}
                 placeholder="Address"
-                onChangeText={formik.handleChange("address")}
-                onBlur={formik.handleBlur("address")}
-                value={formik.values.address}
+                value={address}
+                onChangeText={(e) => setAddress(e)}
                 color="black"
                 
                 
                 // onChangeText={(e) => setPhone(e)}
-               
+                marginBottom={4}
                 fontSize={17}
                 borderBottomColor="#0B0464"
               />
-               {formik.errors.address && formik.touched.address ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.address}
-                </Text>
-              ) : null}
               <FormControl.Label paddingY={2}>
-                <Text fontSize={17} marginTop={4} fontWeight={600}>
+                <Text fontSize={17} fontWeight={600}>
                   Create Password
                 </Text>
               </FormControl.Label>
@@ -510,21 +406,10 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
                 placeholder="********"
                 color="black"
                 fontSize={17}
-                onChangeText={formik.handleChange("password")}
-                onBlur={formik.handleBlur("password")}
-                value={formik.values.password}
+                value={password}
+                onChangeText={(e) => setPassword(e)}
                 borderBottomColor="#0B0464"
               />
-                {formik.errors.password && formik.touched.password ? (
-                <Text
-                  paddingLeft={2}
-                  fontSize="sm"
-                  color="danger.500"
-                  alignSelf="flex-start"
-                >
-                  {formik.errors.password}
-                </Text>
-              ) : null}
             </FormControl>
           </VStack>
         </ScrollView>
@@ -548,7 +433,7 @@ const registerfnt=()=>{console.log("please accept terms & conditions")}
             marginX={10}
             marginTop={2}
             rounded={10}
-            onPress={formik.handleSubmit}
+            onPress={isSelected?registerfn2:registerfnt}
             bg="black"
           >
             <Text marginY={1} color="white" bold fontSize={20}>
